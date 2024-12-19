471e897c3ec73c1a377b068641990361130950cd
Sahana-Lakshmipathy <sahana.lp100@gmail.com>
2024-12-19 19:40:15 +0530

    Docker frontend image creation

--- src/server.cjs
+++ src/server.cjs
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
@@ -129,4 +130,11 @@ app.put("/modify-employee/:id", (req, res) => {
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// Serve the frontend build
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
